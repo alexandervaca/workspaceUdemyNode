dbcb5f80c53177a943b9c39e552f93738357244b
alexandervaca.alexvaca <alexandervaca.alex_andy_25@yahoo.com.ar>
2022-04-28 22:40:10 -0300

    Google SignIn added

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,8 +1,11 @@
 const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 
-const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+
+const Usuario = require('../models/usuario');
+
 
 const login = async(req, res = respose) => {
 
@@ -49,8 +52,56 @@ const login = async(req, res = respose) => {
   }
 }
 
+const googleSignIn = async(req, res = respose) => {
+  
+  const { id_token } = req.body;
 
+  try {
+    const { nombre, img, mail } = await googleVerify(id_token);
+    
+    let usuario = await Usuario.findOne({ mail });
+
+    if( !usuario ){
+      // Crear usuario
+      const data = {
+        nombre,
+        mail,
+        password: ':P',
+        img,
+        google: true
+      };
+
+      usuario = new Usuario( data );
+      await usuario.save();
+    }
+
+    // Si el usuario en DB
+    if( !usuario.estado ){
+      return res.status(401).json({
+        msg: 'Contacte al administrador, usuario bloqueado'
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT( usuario.id );
+
+
+    res.json({
+      usuario,
+      token
+    });
+
+  } catch (error) {
+    console.log('error:',error)
+
+    res.status(400).json({
+      ok: false,
+      msg: 'El Token no se pudo verificar'
+    })
+  }
+}
 
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
--- 07-restserver/routes/auth.js
+++ 07-restserver/routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -11,8 +11,12 @@ router.post('/login', [
   check('mail', 'El mail es obligatorio').isEmail(),
   check('password', 'El password es obligatorio').not().isEmpty(),
   validarCampos
-],login);
+], login);
 
+router.post('/google', [
+  check('id_token', 'El id_token es necesario').not().isEmpty(),
+  validarCampos
+], googleSignIn);
 
 
 module.exports = router;
